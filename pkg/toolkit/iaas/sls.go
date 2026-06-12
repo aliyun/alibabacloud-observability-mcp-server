a11d7283e40b728d1c591d1bfa7949939b7a0b27
jmg02449882 <jmg02449882@alibaba-inc.com>
2026-06-12 09:57:06 +0800

    lint and check

--- pkg/toolkit/iaas/sls.go
+++ pkg/toolkit/iaas/sls.go
@@ -601,11 +601,11 @@ func (h *slsHandler) handleExecuteSQL(ctx context.Context, params map[string]int
 		"limit", limit, "offset", offset, "reverse", reverse)
 
 	results, err := h.slsClient.Query(ctx, regionID, project, logStore, &sls.GetLogsRequest{
-		Query: tea.String(query),
-		From:  tea.Int32(int32(fromTS)),
-		To:    tea.Int32(int32(toTS)),
-		Line:  tea.Int64(int64(limit)),
-		Offset: tea.Int64(int64(offset)),
+		Query:   tea.String(query),
+		From:    tea.Int32(int32(fromTS)),
+		To:      tea.Int32(int32(toTS)),
+		Line:    tea.Int64(int64(limit)),
+		Offset:  tea.Int64(int64(offset)),
 		Reverse: tea.Bool(reverse),
 	})
 	if err != nil {
